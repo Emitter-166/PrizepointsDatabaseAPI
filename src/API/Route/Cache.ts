356a565133692681db27592aec45d96e2e24d999
Emitter <xperimentschaftler@gmail.com>
2022-12-19 21:55:50 +0600

    Bug fixes

--- src/API/Route/Cache.ts
+++ src/API/Route/Cache.ts
@@ -1,54 +1,35 @@
 import {Request, Response} from "express";
-import {pointsTableCache} from "./point";
+import {pointsTableCache, pointsUserCache} from "./point";
 import {gamesCache} from "./game";
 
-export const gameValidator = async (req:Request, res:Response) => {
+export const gameValidator = async (req: Request, res: Response) => {
     const {name, time} = req.query;
-    if(!name || !time){
+    if (!name || !time) {
         res.status(400).send({
             message: "invalid body"
         })
         return
     }
-   try{
-        if(gamesCache.get(name as string) === Number(time)){
-           res.status(200).send({
-               valid: true
-           })
-       }else{
-           res.status(200).send({
-               valid: false
-           })
-       }
-   }catch (err){
-       res.status(500).send({
-           message: "Please make sure your queries are valid"
-       })
-   }
+    res.status(200).send({
+        valid: (gamesCache.get(name as string) === Number(time))
+    })
 }
 
-export const pointsValidator = async (req:Request, res:Response) => {
-    const {name, time} = req.query;
-    if(!name || !time) {
+export const pointsValidator = async (req: Request, res: Response) => {
+    const {name, time, userId} = req.query;
+    if ((!name || !userId) && !time) {
         res.status(400).send({
             message: "invalid body"
         })
         return
     }
-    try{
-        if(pointsTableCache.get(name as string) === Number(time)){
-            res.status(200).send({
-                valid: true
-            })
-        }else{
-            res.status(200).send({
-                valid: false
-            })
-        }
-    }catch (err){
-        res.status(500).send({
-            message: "Please make sure your queries are valid"
+    if (userId) {
+        res.status(200).send({
+            valid: (pointsUserCache.get(userId as string) === Number(time))
+        })
+    } else {
+        res.status(200).send({
+            valid: (pointsTableCache.get(name as string) === Number(time))
         })
     }
-
 }
--- src/API/Route/point.ts
+++ src/API/Route/point.ts
@@ -3,23 +3,59 @@ import {sequelize} from "../../index";
 import {model as pointsModel} from "../../Database/Models/Dynamic/points";
 
 export const pointsTableCache = new Map<string, number>();
+export const pointsUserCache = new Map<string, number>();
 
-export const getPoints =  async (req: Request, res: Response) => {
-    const name = req.query.name as string;
+export const getPoints = async (req: Request, res: Response) => {
+    let {name, userId} = req.query;
+    name = name as string;
+    userId = userId as string;
+
     if (!name) {
         res.status(400).send({message: "invalid body"})
         return;
     }
-    try{
-        const model = await sequelize.model(name).findAll();
-        res.status(200).send({
-            message: "points table found",
-            model,
-            updatedAt: pointsTableCache.get(name)
-        })
-    }catch (err){
-        res.status(400).send({message: "No game points table with that name is found!"})
+    if (userId) {
+        try {
+            const mainModel = sequelize.model(name)
+            const model = await mainModel.findOne({
+                where: {
+                    userId: userId
+                }
+            })
+            if(model !== null){
+                if(!pointsUserCache.has(userId)){
+                    pointsUserCache.set(userId, (new Date()).getTime())
+                }
+                res.status(200).send({
+                    message: "points found",
+                    model,
+                    updatedAt: pointsUserCache.get(userId)
+                })
+            }else{
+                res.status(400).send({
+                    message: "points not found",
+                })
+            }
+
+        } catch (err) {
+            res.status(400).send({message: "No game points table with that name is found!"})
+        }
+    } else {
+        try {
+            if (!pointsTableCache.has(name)) {
+                pointsTableCache.set(name, (new Date()).getTime())
+            }
+            const model = await sequelize.model(name).findAll();
+            res.status(200).send({
+                message: "points table found",
+                model,
+                updatedAt: pointsTableCache.get(name)
+            })
+        } catch (err) {
+            res.status(400).send({message: "No game points table with that name is found!"})
+        }
     }
+
 }
 
 export const setPoints = async (req: Request, res: Response) => {
@@ -29,9 +65,9 @@ export const setPoints = async (req: Request, res: Response) => {
         return;
     }
     let gamePointsModel;
-    try{
+    try {
         gamePointsModel = sequelize.model(queryData.name as string);
-    }catch (err){
+    } catch (err) {
         pointsModel(sequelize, queryData.name as string)
         await sequelize.sync({alter: true})
         gamePointsModel = sequelize.model(queryData.name as string);
@@ -43,14 +79,15 @@ export const setPoints = async (req: Request, res: Response) => {
 
     const updatedAt = (new Date()).getTime();
     pointsTableCache.set(queryData.name as string, updatedAt);
+    pointsUserCache.set(queryData.userId as string, updatedAt);
 
-    if(created){
+    if (created) {
         res.status(200).send({
-            message:"New user created",
+            message: "New user created",
             model,
             updatedAt
         })
-    }else{
+    } else {
         await model.update({
             points: queryData.points
         })
